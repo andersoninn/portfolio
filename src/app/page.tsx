cd6a5acece6ff0bbeb08efb715a331d39db6b022
Anderson Carvalho <andersoninnocencio17@gmail.com>
2024-01-16 19:30:59 +0000

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container m-auto bg-red-100">
       <h1> Hellow World!</h1>
     </main>
-  )
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,81 +1,92 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
-import menu from '../../public/image/menu.svg';
+import menu from '../assets/image/menu.svg';
+import logo from '../assets/image/logo.svg';
 
 import {
-   Navbar,
-   NavbarBrand,
-   NavbarContent,
-   NavbarItem,
-   Link,
-   Button,
-   NavbarMenuToggle,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownItem,
+  DropdownMenu,
 } from '@nextui-org/react';
 import { NextUIProvider } from '@nextui-org/react';
 
-import { FaBars } from 'react-icons/fa6';
 // import { AcmeLogo } from './AcmeLogo.jsx';
 
 export default function Header() {
-   return (
-      <NextUIProvider>
-         <Navbar>
-            <NavbarBrand>
-               <div className="bg-red-200">
-                  <p>OAK</p>
-                  <p>Innovation</p>
-               </div>
-               {/* <AcmeLogo />
+  return (
+    <NextUIProvider>
+      <Navbar className="py-4 container m-auto bg-green-100">
+        <NavbarBrand>
+          <div className="">
+            <Image src={logo} alt="" />
+          </div>
+          {/* <AcmeLogo />
             <p className="font-bold text-inherit">ACME</p> */}
-            </NavbarBrand>
+        </NavbarBrand>
 
-            <NavbarContent className="hidden sm:flex gap-4 " justify="center">
-               <NavbarItem>
-                  <Link
-                     href="#"
-                     className="text-brand-blue100 text-xl font-bold bg-brand-background py-3 px-11 rounded-lg 
+        <NavbarContent className="hidden sm:flex " justify="center">
+          <NavbarItem>
+            <Link
+              href="#"
+              className="text-brand-blue100 text-xl font-bold bg-white py-2 px-11 rounded-lg 
                      hover:drop-shadow-brandShadow hover:text-brand-blue100"
-                  >
-                     Home
-                  </Link>
-               </NavbarItem>
-               <NavbarItem isActive>
-                  <Link
-                     href="#"
-                     aria-current="page"
-                     className="text-brand-blue100 text-xl font-bold bg-brand-background py-3 px-11 rounded-lg 
+            >
+              Home
+            </Link>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <Link
+              href="#"
+              aria-current="page"
+              className="text-brand-blue100 text-xl font-bold bg-white py-2 px-11 rounded-lg 
                      hover:drop-shadow-brandShadow hover:text-brand-blue100 active:bg-red-200 
                      !focus:bg-brand-blue100 !focus:text-brand-background"
-                  >
-                     About
-                  </Link>
-               </NavbarItem>
-               <NavbarItem>
-                  <Link
-                     color="foreground"
-                     href="#"
-                     className="text-brand-blue100 text-xl font-bold bg-brand-background py-3 px-11 rounded-lg 
+            >
+              About
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link
+              color="foreground"
+              href="#"
+              className="text-brand-blue100 text-xl font-bold bg-white py-2 px-11 rounded-lg 
                      hover:drop-shadow-brandShadow hover:text-brand-blue100"
-                  >
-                     Projects
-                  </Link>
-               </NavbarItem>
-            </NavbarContent>
+            >
+              Projects
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
 
-            <NavbarContent justify="end" className="sm:hidden">
-               <NavbarItem>
-                  <Button
-                     as={Link}
-                     href="#"
-                     variant="flat"
-                     className="bg-[#f5f5f5]"
-                  >
-                     <Image src={menu} width={25} alt="" />
-                  </Button>
-               </NavbarItem>
-            </NavbarContent>
-         </Navbar>
-      </NextUIProvider>
-   );
+        <NavbarContent justify="end" className="sm:hidden">
+          <NavbarItem>
+            <Dropdown>
+              <DropdownTrigger>
+                <Button as={Link} href="#" variant="flat" className="bg-white">
+                  <Image src={menu} width={25} alt="" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu aria-label="Static Actions">
+                <DropdownItem
+                  key="new"
+                  className="text-brand-blue100 active:bg-red-200"
+                >
+                  Home
+                </DropdownItem>
+                <DropdownItem key="copy">About</DropdownItem>
+                <DropdownItem key="edit">Projects</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </NextUIProvider>
+  );
 }
