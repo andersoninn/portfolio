1d866d56d3cf682b837196ae0fe093c1e4d2f308
Anderson Carvalho <andersoninnocencio17@gmail.com>
2024-04-16 19:34:29 +0100

    layout Projects page

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,17 +1,75 @@
 import RealProjectsCarousel from './carousel/RealProjectsCarousel';
 import RealProjectsModal from '@/components/projects/modals/RealProjectsModal';
 
+import Mockup from '@/assets/image/projects/mockup.svg';
+import iconLink from '@/assets/image/projects/icon link.svg';
+
+import Image from 'next/image';
+
 export default function Projects() {
   return (
     <>
       <section className="bg-gradient-to-b from-[#D6C4E2] from-10% via-[#fadd99] via-20% to-[#B8AD94] to-70%">
         <section className="w-full">
-          <section className="w-[90%] pt-8 pb-10 flex flex-col m-auto max-w-[1024px]">
-            <h2 className="text-brand-blue100 font-bold">Projects</h2>
-            <h1 className="text-brand-blueTitle text-4xl font-black">
-              Creative Soluctions
-            </h1>
-            <section className="  flex flex-col md:flex-row m-auto gap-12 pt-6 max-w-[99%]">
+          <section className="w-[85%] pt-36 md:pt-32 pb-10 flex flex-col m-auto max-w-[1240px]">
+            <section className="flex flex-col md:flex-row">
+              <article className="m-auto flex flex-col items-center text-center gap-1">
+                <Image src={Mockup} alt="" width={600} />
+                <h1>Escola de Idiomas get Along</h1>
+                <h2 className="font-bold">
+                  Landing page com redirecionamento para outra plataforma
+                  integrada a um chatboot para auto atendimento, configurado
+                  pela equipe.
+                </h2>
+                <a href="" className="flex gap-4 m-auto">
+                  <Image src={iconLink} alt="" width={15} />
+                  <span className="text-blue-600">Veja mais </span>
+                </a>
+              </article>
+              <article className="m-auto flex flex-col items-center text-center gap-1">
+                <Image src={Mockup} alt="" width={600} />
+                <h1>Escola de Idiomas get Along</h1>
+                <h2 className="font-bold">
+                  Landing page com redirecionamento para outra plataforma
+                  integrada a um chatboot para auto atendimento, configurado
+                  pela equipe.
+                </h2>
+                <a href="" className="flex gap-4 m-auto">
+                  <Image src={iconLink} alt="" width={15} />
+                  <span className="text-blue-600">Veja mais </span>
+                </a>
+              </article>
+            </section>
+            <section className="flex flex-col md:flex-row gap-12 mt-12">
+              <article className="m-auto flex flex-col items-center text-center gap-1">
+                <Image src={Mockup} alt="" width={600} />
+                <h1>Escola de Idiomas get Along</h1>
+                <h2 className="font-bold">
+                  Landing page com redirecionamento para outra plataforma
+                  integrada a um chatboot para auto atendimento, configurado
+                  pela equipe.
+                </h2>
+                <a href="" className="flex gap-4 m-auto">
+                  <Image src={iconLink} alt="" width={15} />
+                  <span className="text-blue-600">Veja mais </span>
+                </a>
+              </article>
+              <article className="m-auto flex flex-col items-center text-center gap-1">
+                <Image src={Mockup} alt="" width={600} />
+                <h1>Escola de Idiomas get Along</h1>
+                <h2 className="font-bold">
+                  Landing page com redirecionamento para outra plataforma
+                  integrada a um chatboot para auto atendimento, configurado
+                  pela equipe.
+                </h2>
+                <a href="" className="flex gap-4 m-auto">
+                  <Image src={iconLink} alt="" width={15} />
+                  <span className="text-blue-600">Veja mais </span>
+                </a>
+              </article>
+            </section>
+
+            {/* <section className="  flex flex-col md:flex-row m-auto gap-12 pt-6 max-w-[99%]">
               <section className="p-5 bg-gray-50 rounded-lg flex flex-col gap-3 shadow-md max-w-[450px] overflow-x-hidden">
                 <article className="flex flex-col items-center justify-center overflow-x-hidden">
                   <RealProjectsCarousel />
@@ -33,7 +91,7 @@ export default function Projects() {
                   <RealProjectsModal />
                 </aside>
               </section>
-            </section>
+            </section> */}
           </section>
         </section>
 
@@ -42,5 +100,3 @@ export default function Projects() {
     </>
   );
 }
-
-
