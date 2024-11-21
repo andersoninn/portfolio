38759c47880821068155853fd44b655a701a27a5
Anderson Carvalho <andersoninnocencio17@gmail.com>
2024-11-21 11:31:50 +0000

    instal css animate and create aniamte in home

--- src/components/home/Home2.tsx
+++ src/components/home/Home2.tsx
@@ -1,8 +1,7 @@
 'use client';
+import 'animate.css';
 import Image from 'next/image';
-import HomeLinkedin from '@/assets/image/home/homeLinkedin.svg';
-import HomeGitHub from '@/assets/image/home/homeGithub.svg';
-import HomeCurriculum from '@/assets/image/home/homeCurriculum.svg';
+import { IoArrowDown } from "react-icons/io5";
 
 import { Texts } from '@/lib/Texts';
 import Link from 'next/link';
@@ -12,24 +11,44 @@ export default function Home2() {
   const HomeTextes = Texts.home;
   return (
     <>
-    <NavbarUI></NavbarUI>
+    <NavbarUI/>
       <section className="bg-gradient-to-b from-[#0f9ffe] from-10% via-[#077AED] via-20% to-[#06283B] to-90% h-screen">
         <section
           id="home"
-          className="flex justify-center m-auto pt-36 md:pt-46 pb-52 w-[85%] md:w-[90 %] md:pt-28 lg:w-full 
-          max-w-[1240px]"
+          className="max-w-[1240px] flex flex-col justify-center m-auto" 
         > 
-          <article className="font-extrabold text-center text-white text-4xl md:text-6xl flex flex-col justify-center">
-            <h1 className="">Anderson Carvalho</h1>
-            <div className="flex justify-center w-full text-medium font-normal ">
-              <p className="text-end md:text-lg md:max-w-[400px]">
-                Web and Software Developer
+
+
+          <article className="pt-[60%] font-normal text-center text-white flex flex-col justify-center">
+            <h1 className="text-4xl">Anderson Carvalho</h1>
+            <div className="flex flex-col justify-center w-full font-light text-2xl">
+              <p className="">
+                Web and Software
               </p>
+              <p>Developer</p>
             </div>
           </article>
+          <section className='absolute bottom-10 w-full flex flex-col justify-center'>
+            <article className='relative w-full'>
+              <div className='absolute top-24 w-full flex flex-col m-auto items-center justify-center text-white'>
+                <h2 className='text-2xl font-light'>Turning ideais</h2>
+                <p className='text-3xl font-normal'>into possibilities</p>
+              </div>
+            </article>
+            <Image src="/Iphone15.png" alt='' width={400} height={400}/>
+          </section>
+          <article className="absolute bottom-4 flex w-full justify-center">
+          
+          <span className="animate__animated animate__slideOutDown animate__infinite">
+          <IoArrowDown className='text-white w-6 h-6'/>
+          </span>
+          </article>
         </section>
+        <article className='flex items-center'>
+
+            <Image src="/homePolygons.png" alt='' width={700} height={700} className='absolute bottom-0'/>
+        </article>
       </section>
-      <section className="divisionHome" />
     </>
   );
 }
