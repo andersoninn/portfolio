c173e327afd67501d50eeeb5c8cff8a0360100e9
Anderson Carvalho <andersoninnocencio17@gmail.com>
2024-12-04 21:29:58 +0000

    feat: teste layout

--- src/components/home/Home2.tsx
+++ src/components/home/Home2.tsx
@@ -2,8 +2,8 @@
 import 'animate.css';
 import { Texts } from '@/lib/Texts2';
 
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import NavbarUI from '../ui/NavbarUI';
 import Image from 'next/image';
@@ -13,33 +13,45 @@ export default function Home2() {
   const { ref, inView } = useInView({ threshold: 0.1 });
   return (
     <>
-      <section className="max-w-screen h-full bg-gradient-to-b from-cyan-500 to-blue-500 relative">
-        <NavbarUI />
+      <section className="max-w-screen h-full ">
+        {/* <NavbarUI /> */}
         <section className="bgHome">
           <section className="container w-[90%] m-auto min-h-[700px] pt-[30%] ">
-            <article
-              className="flex flex-col justify-center items-center text-white text-center"
-            >
+            <article className="flex flex-col justify-center items-center text-gray-800 text-center">
               <span>
-                <h1 className='text-4xl font-extrabold'>{texts.name}</h1>
-                <p> {texts.subtitle}</p>
+                <p className="text-xl animate__animated animate__fadeIn animate__delay-2s">
+                  Because there is only one way
+                </p>
+                <p className="text-6xl font-extrabold animate__animated animate__fadeIn">
+                  We'll Make
+                </p>
+                <p className="text-xl animate__animated animate__fadeIn animate__delay-2s">
+                  to make it to the next step
+                </p>
+                <p className="text-6xl font-extrabold animate__animated animate__fadeIn">
+                  {' '}
+                  It work
+                </p>
+                <p className="text-xl animate__animated animate__fadeIn animate__delay-2s">
+                  {' '}
+                  — nailing your mvp —
+                </p>
               </span>
-
-
-
-
-
             </article>
 
-            <article className='absolute -bottom-10 w-[90%] flex flex-col justify-center items-center text-center text-white animate__animated animate__fadeInUp'>
+            <article className="absolute -bottom-10 w-[90%] flex flex-col justify-center items-center text-center text-white animate__animated animate__fadeInUp">
               <span>
-                <h2 className='font-semibold text-xl'>{texts.calToAction}</h2>
+                <h2 className="font-semibold text-xl">{texts.calToAction}</h2>
                 <p>{texts.subtitleCalToAction}</p>
               </span>
-              <Image src="/iphone.png" width={230} height={200} alt="" className='-mt-28' />
-
+              <Image
+                src="/iphone.png"
+                width={230}
+                height={200}
+                alt=""
+                className="-mt-28"
+              />
             </article>
-
           </section>
         </section>
       </section>
--- src/components/ui/NavbarUI.tsx
+++ src/components/ui/NavbarUI.tsx
@@ -6,79 +6,127 @@ import {
   NavbarItem,
   NavbarMenuToggle,
   NavbarMenu,
-  NavbarMenuItem,
   Link,
-  Button,
 } from '@nextui-org/react';
 import { AcmeLogo } from './AcmeLogo.jsx';
-
 import { IoMenuSharp } from 'react-icons/io5';
 import { IoCloseSharp } from 'react-icons/io5';
-import { BsTransparency } from 'react-icons/bs';
+import { motion } from 'framer-motion';
+import { Texts } from '@/lib/Texts2';
+import { TfiLinkedin } from 'react-icons/tfi';
+import {
+  BiLogoGithub,
+  BiLogoGmail,
+  BiLogoWhatsapp,
+  BiSolidDownload,
+} from 'react-icons/bi';
 
 export default function NavbarUI() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = ['HOME', 'ABOUT', 'LOGO', 'PROJECTS', 'CURRICULUM'];
+  const menuItems = ['HOME', 'ABOUT', 'PROJECTS', 'CURRICULUM'];
+  const texts = Texts.footer;
 
   return (
     <Navbar
       shouldHideOnScroll
       onMenuOpenChange={setIsMenuOpen}
       isBlurred={true}
-      className=" bg-white shadow-lg"
+      className="bg-white"
     >
-      <NavbarContent>
-        <span className="flex justify-between w-full">
-          <NavbarBrand className="text-gray-800 max-w-20 md:hidden">
-            <AcmeLogo />
-            <p className="font-bold text-inherit">LOGO</p>
-          </NavbarBrand>
+      <NavbarContent
+        className={`w-full flex justify-between items-center ${
+          !isMenuOpen && 'bg-[#2C373D]'
+        }`}
+      >
+        {/* Logo no lado esquerdo */}
+        <NavbarBrand className="text-gray-800">
+          <p className="font-bold text-inherit text-white">LOGO</p>
+        </NavbarBrand>
+
+        {/* Menu toggle no lado direito (aparece apenas em dispositivos móveis) */}
+        <motion.div
+          initial={{ backgroundColor: '#2d3748' }} // Cor inicial (gray-800)
+          whileHover={{
+            backgroundColor: ['#dfdf65', '#20575d'], // Transição de amarelo para verde
+            transition: { duration: 0.5, ease: 'easeInOut' },
+          }}
+          animate={{
+            backgroundColor: '#2d3748', // Retorna para gray-800 após o hover
+            transition: { duration: 0.5, ease: 'easeInOut' },
+          }}
+          className="w-12 h-12 rounded-full text-white flex items-center justify-center"
+        >
           <NavbarMenuToggle
             aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-            className="sm:hidden mr-2"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
             icon={
               isMenuOpen ? (
-                <IoCloseSharp className="w-12 h-12 text-gray-800" />
+                <IoCloseSharp className="w-8 h-8" />
               ) : (
-                <IoMenuSharp className="w-12 h-12 text-gray-800" />
+                <IoMenuSharp className="w-8 h-8" />
               )
             }
           />
-        </span>
+        </motion.div>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden w-full justify-between gap-4 md:w-full md:flex md:justify-around"
-        justify="center"
+      {/* Menu de navegação */}
+      <NavbarMenu
+        aria-hidden={!isMenuOpen}
+        className="w-full max-h-[600px] pt-6 bg-[#2C373D] items-center  rounded-b-2xl"
       >
-        <span className="w-full flex justify-around items-center text-gray-800">
-          {menuItems.map((item) => (
-            <NavbarItem key={item} className="font-medium">
-              {item === 'LOGO' ? (
-                <NavbarBrand>
-                  <AcmeLogo />
-                  <p className="font-bold text-inherit">LOGO</p>
-                </NavbarBrand>
-              ) : (
-                <Link className="text-gray-800" href="#">
-                  {item}
+        {menuItems.map((item) => (
+          <NavbarItem key={item} className="px-4 py-2 font-inter">
+            <Link
+              className="text-white hover:text-[#21585E] text-4xl font-extrabold"
+              href="#"
+            >
+              {item}
+            </Link>
+          </NavbarItem>
+        ))}
+        <section className="flex flex-col items-center gap-4 mb-12">
+          <p className="text-gray-400">Socials</p>
+          <article className="flex gap-4">
+            {texts.contacts.map((e, i) => (
+              <>
+                <Link
+                  key={i}
+                  className="w-12 h-12 rounded-full border border-white flex justify-center items-center text-white hover:border-[#21585E] hover:bg-white hover:text-[#21585E] shadow-md cursor-pointer "
+                  href={e.href}
+                  target="_blank"
+                >
+                  {e.alt === 'linkedin' && <TfiLinkedin className="w-5 h-5" />}
+                  {e.alt === 'github' && <BiLogoGithub className="w-6 h-6" />}
+                  {e.alt === 'curriculum' && (
+                    <BiSolidDownload className="w-6 h-6" />
+                  )}
+                  {e.alt === 'gmail' && <BiLogoGmail className="w-5 h-5" />}
+                  {e.alt === 'whatsapp' && (
+                    <BiLogoWhatsapp className="w-6 h-6" />
+                  )}
+
+                  {/* <Image src={e.image} height={40} width={40} alt={e.alt} /> */}
                 </Link>
-              )}
-            </NavbarItem>
-          ))}
-        </span>
-      </NavbarContent>
-      <NavbarMenu className="pt-6 gap-4 items-end bg-white">
-        <>
-          {menuItems.map((item) => (
-            <NavbarItem key={item} className={`${item == 'LOGO' && 'hidden'} `}>
-              <Link className="font-normal text-gray-900" href="#">
-                {item}
-              </Link>
-            </NavbarItem>
-          ))}
-        </>
+                {/* <Link
+                    key={i}
+                    className="cursor-pointer rounded-xl hover:-mt-2 shadow-md shadow-black/30"
+                    href={e.href}
+                    target="_blank"
+                  >
+                    <Image src={e.image} height={40} width={40} alt={e.alt} />
+                  </Link> */}
+              </>
+            ))}
+          </article>
+        </section>
+
+        <section className="flex flex-col items-end justify-end pb-6 text-white bg-green-900/30 w-full h-1/2">
+        
+          <span>Local de onde você está acessando</span>
+          <span> WEC DEC 04 2024</span>
+          <span>16:17</span>
+        </section>
       </NavbarMenu>
     </Navbar>
   );
