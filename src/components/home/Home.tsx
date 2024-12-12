e5755673a0cbdd87abb1f9d15847a94150305e73
Anderson Carvalho <andersoninnocencio17@gmail.com>
2024-12-12 15:03:07 +0000

    feat: ajdjust in accessibility

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,6 +2,7 @@
 import 'animate.css';
 import { Texts } from '@/lib/Texts';
 import { HiArrowDown } from 'react-icons/hi2';
+
 import NavbarUI from '../ui/NavbarUI';
 
 export default function Home() {
@@ -14,29 +15,31 @@ export default function Home() {
           <section className="container w-[90%] my-16 m-auto min-h-[700px] md:min-h-[1000px] flex flex-col items-center justify-around md:pb-16">
             {/* H1 */}
             <article className="flex flex-col justify-center items-center text-black text-center">
-              <p className="text-xl md:text-3xl lg:text-4xl animate__animated animate__fadeIn animate__delay-1s mb-2">
-                {texts.subtitle1}
-              </p>
-              <p className="text-6xl md:text-9xl lg:text-[200px] text-nowrap font-extrabold animate__animated animate__fadeIn">
-                {texts.title1}
-              </p>
-              <p className="text-xl md:text-3xl lg:text-4xl animate__animated animate__fadeIn animate__delay-2s md:mt-2 md:-mb-2">
-                {texts.subtitle2}
-              </p>
-              <p className="text-6xl md:text-9xl lg:text-[200px] font-extrabold animate__animated animate__fadeIn">
-                {' '}
-                {texts.title2}
-              </p>
-              <p className="text-xl md:text-3xl lg:text-4xl animate__animated animate__fadeIn animate__delay-3s">
-                {' '}
-                {texts.subtitle3}
-              </p>
+              <span>
+                <p className="text-xl md:text-3xl lg:text-4xl animate__animated animate__fadeIn animate__delay-1s mb-2">
+                  {texts.subtitle1}
+                </p>
+                <p className="text-6xl md:text-9xl lg:text-[200px] text-nowrap font-extrabold animate__animated animate__fadeIn">
+                  {texts.title1}
+                </p>
+                <p className="text-xl md:text-3xl lg:text-4xl animate__animated animate__fadeIn animate__delay-2s md:mt-2 md:-mb-2">
+                  {texts.subtitle2}
+                </p>
+                <p className="text-6xl md:text-9xl lg:text-[200px] font-extrabold animate__animated animate__fadeIn">
+                  {' '}
+                  {texts.title2}
+                </p>
+                <p className="text-xl md:text-3xl lg:text-4xl animate__animated animate__fadeIn animate__delay-3s">
+                  {' '}
+                  {texts.subtitle3}
+                </p>
+              </span>
             </article>
 
             <article
               className="flex justify-center items-center md:pt-12 lg:pt-0"
             >
-              <HiArrowDown className="w-6 h-6 hidden" />
+              <HiArrowDown className="w-6 h-6  lg:w-8 lg:h-8 animate__animated animate__fadeIn animate__delay-4s text-black/0" />
             </article>
             <article className="w-[180px] h-[180px] md:w-[300px] md:h-[300px] overflow-hidden absolute right-0 -bottom-[77px] md:-bottom-[144px]">
             </article>
