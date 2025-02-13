dc9884a307826e979ca41a2d463a3bf252b3cfd4
Anderson Carvalho <andersoninnocencio17@gmail.com>
2025-02-13 09:05:07 +0000

    feat: remove mx from ProjectItens

--- src/components/ui/NewNavbar.tsx
+++ src/components/ui/NewNavbar.tsx
@@ -131,7 +131,7 @@ export default function NewNavbar() {
                       >
                         {item.name}
 
-                        <span className="absolute bottom-0 left-1/2 w-0 h-[2px] bg-[#21585E] transition-all duration-300 group-hover:w-full group-hover:left-0" />
+                        <span className="absolute bottom-0 left-1/2 w-0 h-[3px] rounded-xl bg-[#21585E] transition-all duration-300 group-hover:w-full group-hover:left-0" />
                       </Link>
                     </motion.div>
                   </>
